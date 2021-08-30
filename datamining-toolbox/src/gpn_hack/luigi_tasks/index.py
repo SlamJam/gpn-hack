7650ce670643c2fdf23c68f2b2066f3e672ca6d6
Anton Shurpin <anton.shurpin@gmail.com>
2021-08-30 03:10:04 +0300

    Add EES indexing. Add similar, add source.

--- datamining-toolbox/src/gpn_hack/luigi_tasks/index.py
+++ datamining-toolbox/src/gpn_hack/luigi_tasks/index.py
@@ -1,5 +1,8 @@
+import itertools as it
 import tempfile
+import typing
 
+import elastic_enterprise_search as ees
 import elasticsearch as es
 import elasticsearch.exceptions
 import luigi
@@ -7,7 +10,7 @@ import luigi.contrib.s3
 import luigi.format
 import luigi.mock
 import more_itertools as mit
-from elastic_enterprise_search import AppSearch
+import pydantic
 from elasticsearch.client import CatClient
 from elasticsearch.helpers import bulk
 
@@ -15,14 +18,16 @@ from .. import embedder, qdrant, utils
 from . import hh
 from .common import DEFAULT_HH_AREAS, ELASTICSEARCH_URL, QDRANT_URL
 
-EES_ENGINE_NAME = "gazpromneft"
+EES_ENGINE_NAME = "gazpromneft1"
 EES_URL = "https://gazpromneft.ent.eastus2.azure.elastic-cloud.com"
 EES_AUTH_KEY = "private-4xzeh815k7k5ffooxy4va621"
+INDEX_EES = False
 
 
 COMPANIES_COLLECTION = "companies"
 QDRANT_COLLECTION = "companies"
 FASTTEXT_MODEL_KEY = "s3://fasttext-model/ft_model_small.bin"
+ENERGYBASE_DATA_KEY = "s3://energybase/result.jsonl.gz"
 
 
 class PrepareEmbedderModel(luigi.Task):
@@ -30,15 +35,114 @@ class PrepareEmbedderModel(luigi.Task):
         return luigi.contrib.s3.S3Target(FASTTEXT_MODEL_KEY, format=luigi.format.Nop)
 
 
-def iter_over_gas_oil(f):
+class PrepareEnergybaseData(luigi.Task):
+    def output(self):
+        return luigi.contrib.s3.S3Target(ENERGYBASE_DATA_KEY, format=luigi.format.Gzip)
+
+
+class Company(pydantic.BaseModel):
+    # id: int
+    name: str
+    description: typing.Optional[str] = None
+
+    email: str = ""
+    phone: str = ""
+    site_url: str = ""
+
+    industries: typing.List[str] = []
+
+    # ---
+    source: str
+
+
+class IndexItem(pydantic.BaseModel):
+    id: int
+    company: Company
+    descriptor: typing.Optional[typing.List[float]] = None  # np.ndarray
+    similar_ids: typing.List[int] = []
+
+    @property
+    def qdrant_point(self):
+        assert self.descriptor, "descriptor must be not None"
+        return {"id": self.id, "vector": self.descriptor}
+
+    @property
+    def elastic_doc(self):
+        doc = self.company.dict()
+        doc["_id"] = self.id
+        doc["similar_ids"] = self.similar_ids
+
+        return doc
+
+
+def iter_hh_area_data(f) -> typing.List[Company]:
     data = utils.iter_over_jsonl(f)
 
     # 47 - отрасль "Нефть и газ"
-    yield from filter(
+    data = filter(
         lambda c: any(
             ind_id == "47" or ind_id.startswith("47.") for ind in c["industries"] if (ind_id := ind["id"]) or True
         ),
         data,
+    )
+
+    def fix(c):
+        c["source"] = "hh"
+
+        c["industries"] = [ind["name"] for ind in c["industries"]]
+
+        if c["site_url"] == "http://":
+            c.pop("site_url")
+
+        return c
+
+    data = map(fix, data)
+
+    return map(Company.parse_obj, data)
+
+
+def iter_energybase_data(f) -> typing.List[Company]:
+    data = utils.iter_over_jsonl(f)
+
+    def fix(c):
+        c["source"] = "energybase"
+
+        attrs = c["attributes"]
+        c["email"] = attrs["email"]
+        c["phone"] = attrs["phone"]
+        c["site_url"] = attrs["site"]
+
+        return c
+
+    data = map(fix, data)
+
+    return map(Company.parse_obj, data)
+
+
+def create_indexitem(ft_model, num: int, company: Company) -> IndexItem:
+    descriptor = None
+
+    if company.description:
+        # TODO: split ',', '.', ' '
+        descriptor = [float(num) for num in embedder.sema_embedder(company.description.split(), ft_model)]
+
+    return IndexItem(id=num, company=company, descriptor=descriptor)
+
+
+def index_es(es_client, chunk):
+    bulk(client=es_client, index=COMPANIES_COLLECTION, actions=(c.elastic_doc for c in chunk))
+
+
+def index_ees(ees_client, chunk):
+    def fix(doc):
+        # EES reject fields which start with "_"
+        doc["id"] = doc.pop("_id")
+
+        return doc
+
+    ees_client.index_documents(
+        engine_name=EES_ENGINE_NAME,
+        documents=[fix(c.elastic_doc) for c in chunk],
     )
 
 
@@ -46,88 +150,98 @@ class IndexHHArea(luigi.Task):
     area_id = luigi.IntParameter()
 
     def requires(self):
-        return PrepareEmbedderModel(), hh.HHClearCompaniesDescriptionsAtArea(self.area_id)
+        return PrepareEmbedderModel(), hh.HHClearCompaniesDescriptionsAtArea(self.area_id), PrepareEnergybaseData()
 
     def run(self):
-        es_client = es.Elasticsearch(ELASTICSEARCH_URL)
-        es_cat_client = CatClient(es_client)
+        if INDEX_EES:
+            ees_client = ees.AppSearch(EES_URL, http_auth=EES_AUTH_KEY)
 
-        ees_client = AppSearch(EES_URL, http_auth=EES_AUTH_KEY)
+            try:
+                ees_client.get_engine(engine_name=EES_ENGINE_NAME)
+                # ees_client.delete_engine(engine_name=EES_ENGINE_NAME)
+            except ees.NotFoundError:
+                pass
+            else:
+                print(f"Engine {EES_ENGINE_NAME} already exists. Nothing to do.")
+                return
+        else:
+            es_client = es.Elasticsearch(ELASTICSEARCH_URL)
+            es_cat_client = CatClient(es_client)
 
-        # Check existing index
-        # try:
-        #     es_cat_client.indices(index=COMPANIES_COLLECTION)
-        # except elasticsearch.exceptions.NotFoundError:
-        #     pass
-        # else:
-        #     print(f"Collection {COMPANIES_COLLECTION} already exists. Nothing to do.")
-        #     self.mark_sucess()
-        #     return
+            # Check existing index
+            try:
+                es_cat_client.indices(index=COMPANIES_COLLECTION)
+            except elasticsearch.exceptions.NotFoundError:
+                pass
+            else:
+                print(f"Collection {COMPANIES_COLLECTION} already exists. Nothing to do.")
+                self.mark_sucess()
+                return
 
-        fasttext_model, area_input = self.input()
+        fasttext_model, hh_area_input, energybase_input = self.input()
 
-        # # Prepare Embedder
+        # ===== Prepare Embedder =====
 
-        # # We neeeeed fileNAME, copy data to temporary file... with name
-        # with tempfile.NamedTemporaryFile() as f:
-        #     with fasttext_model.open() as f_mdl:
-        #         buf_size = 1 * 1024 * 1024
-        #         while len(buf := f_mdl.read(buf_size)) != 0:
-        #             f.write(buf)
-        #         f.seek(0)
+        # We neeeeed fileNAME, copy data to temporary file... with name
+        with tempfile.NamedTemporaryFile() as f:
+            with fasttext_model.open() as f_mdl:
+                buf_size = 1 * 1024 * 1024
+                while len(buf := f_mdl.read(buf_size)) != 0:
+                    f.write(buf)
+                f.seek(0)
 
-        #     ft_model = embedder.load_gensim_model(f.name)
+            ft_model = embedder.load_gensim_model(f.name)
 
-        # # Prepare Qdrant
-        # qdr = qdrant.QIndexer(url=QDRANT_URL)
-        # qdr.drop_collection(QDRANT_COLLECTION)
-        # qdr.create_collection(QDRANT_COLLECTION)
+        # ===== Indexing Qdrant =====
 
-        # # Indexing Qdrant
-        # with area_input.open() as f:
-        #     data = iter_over_gas_oil(f)
+        with hh_area_input.open() as hh_f, energybase_input.open() as eb_f:
+            hh_data = iter_hh_area_data(hh_f)
+            energybase_data = iter_energybase_data(eb_f)
 
-        #     def company_to_qdrant_point(doc):
-        #         desc = doc.get("description") or "нет описания"
+            all_iter_data = it.chain(hh_data, energybase_data)
 
-        #         # TODO: split ',', '.', ' '
-        #         descriptor = embedder.sema_embedder(desc.split(), ft_model)
-        #         return {"id": doc["id"], "vector": [float(num) for num in descriptor]}
+            index_data = [create_indexitem(ft_model, i + 1, c) for i, c in enumerate(all_iter_data)]
 
-        #     chunk_size = 1
-        #     for i, chunk in enumerate(mit.chunked(map(company_to_qdrant_point, data), chunk_size)):
-        #         print("Qdrant | Load chunk:", i, "docs:", i * chunk_size + len(chunk))
-        #         print(chunk)
-        #         qdr.bulk_load(QDRANT_COLLECTION, chunk)
+        print("Index items count:", len(index_data))
 
-        # Indexing Elastic
+        # ===== Prepare Qdrant =====
 
-        with area_input.open() as f:
-            data = iter_over_gas_oil(f)
+        qdr = qdrant.QIndexer(url=QDRANT_URL)
+        qdr.drop_collection(QDRANT_COLLECTION)
+        qdr.create_collection(QDRANT_COLLECTION)
 
-            def company_to_es_doc(c):
-                return {
-                    # EES reject fields which start with "_"
-                    # "_id": c["id"],
-                    "id": c["id"],
-                    "name": c["name"],
-                    "email": "",
-                    "phone": "",
-                    "site_url": c["site_url"],
-                    "description": c["description"],
-                    "industries": [ind["name"] for ind in c.get("industries", [])],
-                }
+        # ===== Indexing Qdrant =====
 
-            chunk_size = 100
-            for i, chunk in enumerate(mit.chunked(map(company_to_es_doc, data), chunk_size)):
-                print("Elastic | Load chunk:", i, "docs:", i * chunk_size + len(chunk))
-                # bulk(client=es_client, index="companies", actions=chunk)
-                ees_client.index_documents(
-                    engine_name=EES_ENGINE_NAME,
-                    documents=chunk,
-                )
+        chunk_size = 1000
+        for i, chunk in enumerate(mit.chunked(filter(lambda item: item.descriptor, index_data), chunk_size)):
+            print("Qdrant | Load chunk:", i, "docs:", i * chunk_size + len(chunk))
+            # print(chunk)
+            qdr.bulk_load(QDRANT_COLLECTION, (c.qdrant_point for c in chunk))
 
-            self.mark_sucess()
+        for item in index_data:
+            if not item.descriptor:
+                continue
+
+            item.similar_ids = qdr.similar_topn(QDRANT_COLLECTION, item.descriptor, 5)
+
+        # ===== Indexing Elastic =====
+
+        if INDEX_EES:
+            ees_client.create_engine(
+                engine_name=EES_ENGINE_NAME,
+                language="ru",
+            )
+
+        chunk_size = 100
+        for i, chunk in enumerate(mit.chunked(index_data, chunk_size)):
+            print("Elastic | Load chunk:", i, "docs:", i * chunk_size + len(chunk))
+
+            if INDEX_EES:
+                index_ees(ees_client, chunk)
+            else:
+                index_es(es_client, chunk)
+
+        self.mark_sucess()
 
     def mark_sucess(self):
         with self.output().open("w"):
--- datamining-toolbox/src/gpn_hack/qdrant.py
+++ datamining-toolbox/src/gpn_hack/qdrant.py
@@ -1,73 +1,44 @@
-import requests
-from typing import Union, List, Dict
-from random import random
+from typing import Dict, List
 
-# import embedder
+import requests
+
+
 Vector = List[float]
 
 
 class QIndexer:
-
     def __init__(self, url, vector_size=1200):
         self.url = url
         self.vector_size = vector_size
 
     def create_collection(self, name):
-        payload = {
-            "create_collection": {
-                "name": name,
-                "vector_size": self.vector_size,
-                "distance": "Cosine"
-            }
-        }
+        payload = {"create_collection": {"name": name, "vector_size": self.vector_size, "distance": "Cosine"}}
 
-        r = requests.post(self.url + '/collections', json=payload)
+        r = requests.post(self.url + "/collections", json=payload)
         r.raise_for_status()
 
     def drop_collection(self, name):
-        payload = {
-            "delete_collection": name
-        }
+        payload = {"delete_collection": name}
 
-        r = requests.post(self.url + '/collections', json=payload)
+        r = requests.post(self.url + "/collections", json=payload)
         # r.raise_for_status()
-
-    def embed(self, text: str) -> Vector:
-        return [random() for x in range(1200)]
-
-    def form_batch(self, docs_list: List[Dict], batch_size: int) -> List[Dict]:
-        for i in range(0, len(docs_list) - 1, batch_size):
-            yield docs_list[i: i + batch_size]
-
-    # def form_point(self, doc: Dict) -> Dict:
-    #     return {
-    #         "id": doc["id"],
-    #         "vector": self.embed(document["description"])
-    #     return result
 
     def bulk_load(self, collection, points: List[Dict]):
         url = self.url + "/collections/" + collection
 
-        payload = {"upsert_points": {"points": points}}
+        payload = {"upsert_points": {"points": list(points)}}
         r = requests.post(url, json=payload)
         r.raise_for_status()
 
+    def similar_topn(self, collection, vector: Vector, n):
+        payload = {
+            "vector": vector,
+            "top": n,
+        }
 
+        url = self.url + f"/collections/{collection}/points/search"
+        r = requests.post(url, json=payload)
+        r.raise_for_status()
 
-
-
-# indexer = QIndexer()
-
-# print(indexer.bulk_load(1, "localhost:6333", "companies", data))
-
-# vect = [random() for i in range(1200)]
-# qur = {
-#     "filter": {"must": [{"key": "industries", "match": {"keyword": "res"}}]},
-#     "vector": vect,
-#     "top": 10,
-# }
-# print(
-#     requests.post(
-#         "http://localhost:6333/collections/companies/points/search", json=qur
-#     ).json()
-# )
+        data = r.json()
+        return [d["id"] for d in data["result"]]
